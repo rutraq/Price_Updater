d79a5890954c1722268b164a41e20455a4a254d3
rutraq <a3332133@gmail.com>
2020-04-03 12:18:54 +0300

    Csv

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("sosi")
+import csv
+
+
+def read_file(file_name):
+    reader = csv.reader(file_name)
+    print(list(reader))
+
+
+if __name__ == "__main__":
+    file = "1.csv"
+    with open(file) as obj:
+        read_file(obj)
